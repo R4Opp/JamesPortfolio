540bb458b6254b40886e517a0786519940286873
James Hudson <jhudson2175@gmail.com>
2019-08-19 11:36:39 -0400

    updated gemfile for bootstrap dependency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = JamesPortViewTool::Renderer.copyright 'James Hudson', 'All right reserved'
+  end
 end
