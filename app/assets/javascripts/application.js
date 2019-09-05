d2fc4cafd39403c23e52ae03707bbd36f46a93cc
James Hudson <jhudson2175@gmail.com>
2019-09-05 12:50:04 -0400

    implemented sortable js library and jquery ui gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
+//= require jquery3
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require jquery-ui
+//= require html.sortable
+//= require popper
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
