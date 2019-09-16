103bf11aa4f44f4a3539894203132cf09892fb19
James Hudson <jhudson2175@gmail.com>
2019-09-16 12:05:12 -0400

    created image background and installs carrierwace minimagick and dotenv gems.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,8 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+#assets for video tag
+Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
@@ -14,3 +16,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( blogs.scss )
 Rails.application.config.assets.precompile += %w( portfolios.scss )
+
+
